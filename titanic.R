5aa6cb491e31beacc2c7dc2377e0919100f887e4
Sohini Palit <sohiniplt@gmail.com>
2018-11-25 09:21:38 -0500

    Update titanic.R

--- titanic.R
+++ titanic.R
@@ -30,12 +30,24 @@ table(train$Sex, train$Survived)
 #examining survival proportion based on gender 
 prop.table(table(train$Sex, train$Survived))
 
+prop.table(table(train$Sex, train$Survived),1)
+
+# using '1', we group by the first dimension which is train$sex
+prop.table(table(train$Sex, train$Survived),1)
+
+# visualization
+train$Sex<-as.factor(train$Sex)
+train$Survived<-as.factor(train$Survived)
+plot(train$Sex, train$Survived)
+
+
 #Updating Survived column in test dataset
 for(i in 1:nrow(test)) {
 if(test$Sex[i] == 'female') {
   test$Survived[i] <-1
 }
 }
+
 
 #Write submission file
 submission2 <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
@@ -44,16 +56,26 @@ write.csv(submission2, file = "Prediction2.csv", row.names = FALSE)
 #Displaying first few rows
 head(test)
 
+# Replacing missing age with Average age
+train[is.na(train[,'Age']), 'Age'] <- mean(train[,'Age'], na.rm = TRUE)
+
 #Creating a new column in test dataset
 train$Child[train$Age < 18] <- "Child"
 train$Child[train$Age > 18] <- "Adult"
 
 #examining survival proportion based on age(Child) 
 prop.table(table(train$Child, train$Survived))
+prop.table(table(train$Child, train$Survived), 1)
 
-#test$Survived[test$Age < 18]<-0
-#submission3 <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
-#write.csv(submission3, file = "Prediction3.csv", row.names = FALSE)
+# visualization
+train$Child<-as.factor(train$Child)
+train$Survived<-as.factor(train$Survived)
+plot(train$Child, train$Survived)
+
+
+test$Survived[test$Age < 18]<-1
+submission3 <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submission3, file = "Prediction3.csv", row.names = FALSE)
 
 summary(train$Fare)
 prop.table(table(train$Fare<14, train$Sex, train$Survived))
@@ -64,3 +86,44 @@ aggregate(train$Survived, by= list(train$Sex, train$Fare<15), FUN=sum)
 # write.csv(submission4, file = "Prediction4.csv", row.names = FALSE)
 
 prop.table(table(train$Pclass, train$Sex, train$Survived))
+
+install.packages('party')
+library(party)
+# png(file = "titanic_decision_tree.png")
+titanic_decision_tree <- ctree(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data = train)
+plot(titanic_decision_tree)
+#dev.off()
+
+
+# Classification Tree with rpart
+install.packages('rpart')
+library(rpart)
+
+# grow tree 
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, method="class",  data = train)
+
+printcp(fit) # display the results 
+plotcp(fit) # visualize cross-validation results 
+summary(fit) # detailed summary of splits
+
+# plot tree 
+plot(fit, uniform=TRUE, main="Classification Tree for titanic")
+text(fit, use.n=TRUE, all=TRUE, cex=.8)
+
+# create attractive postscript plot of tree 
+post(fit, file = "tree.ps", title = "Classification Tree for titanic")
+
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+fancyRpartPlot(fit)
+
+
+Prediction <- predict(fit, test, type = "class")
+
+
+submission4 <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+write.csv(submission4, file = "Prediction4.csv", row.names = FALSE)
